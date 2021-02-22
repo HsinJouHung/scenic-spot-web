b3986f7cf3f3c0d74cfe4415e94c0b2da23d44f3
HsinJouHung <45786914+HsinJouHung@users.noreply.github.com>
2021-02-22 16:44:30 +0800

    init

---
+++ src/actions.jsx
@@ -0,0 +1,8 @@
+import * as types from './types'
+
+// export function setProjectName(name) {
+//     return {
+//         type: types.SET_PROJECT_NAME,
+//         payload: name
+//     }
+// }
---
+++ src/reducer.jsx
@@ -0,0 +1,14 @@
+import * as types from './types'
+
+const initState = {
+
+}
+
+const reducer = (state = initState, {type, payload}) => {
+    switch(type) {
+        default:
+            return state
+    }
+}
+
+export default reducer
