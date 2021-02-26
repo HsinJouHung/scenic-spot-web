29b8f76729a8b1994ca653e7f6b893c07ad34e13
HsinJouHung <45786914+HsinJouHung@users.noreply.github.com>
2021-02-26 21:55:02 +0800

    Table and drawer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useDispatch } from 'react-redux'
 import { setScenicSpotList } from "../actions";
+import SpotInfoTable from './SpotInfoTable'
+import CityDrawer from './CityDrawer'
 
 function Home() {
 
@@ -14,19 +16,21 @@ function Home() {
 
 
   React.useEffect(() => {
-    // dispatch(setScenicSpotList(['ppp', '???']))
     fetch('https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON', requestOptions)
       .then(res => res.json())
       .then(data => {
         console.log('data', data)
         dispatch(setScenicSpotList(data))
+        localStorage.setItem('data', data)
       })
       .catch(error => console.error('Error:', error))
   })
 
   return (
     <div className="home">
-      <h1>hello world!</h1>
+      <CityDrawer /> 
+      {/* <h1>hello world!</h1> */}
+      {/* <SpotInfoTable /> */}
     </div>
   );
 }
