f0c2daa18c393fcf1552ef536e9a2d138d12c304
HsinJouHung <45786914+HsinJouHung@users.noreply.github.com>
2021-03-09 18:59:51 +0800

    Add small table

--- src/components/SpotInfoTable.jsx
+++ src/components/SpotInfoTable.jsx
@@ -9,7 +9,6 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
@@ -23,6 +22,11 @@ const useRowStyles = makeStyles({
             borderBottom: 'unset',
         },
     },
+    smallTable: {
+        '& > *': {
+            paddingLeft: '100px',
+        },
+    }
 });
 
 function Row(props) {
@@ -30,7 +34,7 @@ function Row(props) {
     const [open, setOpen] = React.useState(false);
     const classes = useRowStyles();
     const description = row.Description !== undefined ? row.Description : row.DescriptionDetail
-    
+
 
     return (
         <React.Fragment>
@@ -45,38 +49,26 @@ function Row(props) {
                 </TableCell>
                 <TableCell>{description}</TableCell>
             </TableRow>
-            <TableRow>
+            <TableRow className={classes.smallTable}>
                 <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
                     <Collapse in={open} timeout="auto" unmountOnExit>
-                        <Box margin={1}>
-                            <Typography variant="h6" gutterBottom component="div">
-                                History
-              </Typography>
                             <Table size="small" aria-label="purchases">
                                 <TableHead>
                                     <TableRow>
-                                        <TableCell>Date</TableCell>
-                                        <TableCell>Customer</TableCell>
+                                        <TableCell>Open Time</TableCell>
+                                        <TableCell>Address</TableCell>
+                                        {/* <TableCell>Customer</TableCell>
                                         <TableCell align="right">Amount</TableCell>
-                                        <TableCell align="right">Total price ($)</TableCell>
+                                        <TableCell align="right">Total price ($)</TableCell> */}
                                     </TableRow>
                                 </TableHead>
-                                {/* <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody> */}
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell component="th" scope="row" width='200px'>{row.OpenTime}</TableCell>
+                                        <TableCell component="th" scope="row">{row.Address}</TableCell>
+                                    </TableRow>
+                                </TableBody>
                             </Table>
-                        </Box>
                     </Collapse>
                 </TableCell>
             </TableRow>
@@ -117,9 +109,9 @@ export default function SpotInfoTable(props) {
     const cityName = useSelector(state => state.currCity)
     const cityPath = cityName === '' ? '' : `/${cityName}`
 
-    const getSpots = () => { 
+    const getSpots = () => {
         fetch(`https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot${cityPath}?$top=30&$format=JSON`, requestOptions)
-        // fetch('https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON', requestOptions)
+            // fetch('https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON', requestOptions)
             .then(res => res.json())
             .then(data => {
                 console.log('data', data)
